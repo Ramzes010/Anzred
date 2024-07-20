e9aa7c58d23f33e13a0dd8ce28037bb9c57f872b
Ramzes010 <75881006+Ramzes010@users.noreply.github.com>
2024-07-20 16:52:34 +0300

    123

--- JS/swipe.js
+++ JS/swipe.js
@@ -1,18 +1,21 @@
-const swiper = new Swiper('.team_flex', {
-  // Настройка Swiper
-  slidesPerView: 'auto',
-  slideWidth: 100, // Ширина каждого слайда
-  spaceBetween: 10, // margin-right
-  slideClass: 'team_container', // Добавляем класс для стилизации слайдов
-  breakpoints: {
-    1000: {
-      slidesPerView: 4,  // количество слайдов
-      slideWidth: 400,
-      spaceBetween: 20 //  margin-right
-    }
-  },
-  navigation: {
-    nextEl: '.title_icon img:last-child',
-    prevEl: '.title_icon img:first-child'
+const teamFlex = document.querySelector('.team_flex');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+const teamContainers = document.querySelectorAll('.team_container');
+const containerWidth = teamContainers[0].offsetWidth + 48; // Ширина контейнера + отступ
+
+prevBtn.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    teamFlex.scrollBy(-containerWidth, 0);
+  }
+});
+
+nextBtn.addEventListener('click', () => {
+  if (currentIndex < teamContainers.length - 1) {
+    currentIndex++;
+    teamFlex.scrollBy(containerWidth, 0);
   }
 });
