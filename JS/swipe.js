9e50991f5a6ec8c001c6b9dabdc51d8615bf74fa
Katanoisi <75881006+Ramzes010@users.noreply.github.com>
2024-07-25 11:24:58 +0300

    Update
    - Added swipe effect for team container

--- JS/swipe.js
+++ JS/swipe.js
@@ -1,21 +1,11 @@
-const teamFlex = document.querySelector('.team_flex');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+const container = document.querySelector('.team__container');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
 
-let currentIndex = 0;
-const teamContainers = document.querySelectorAll('.team_container');
-const containerWidth = teamContainers[0].offsetWidth + 48; // Ширина контейнера + отступ
-
-prevBtn.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    teamFlex.scrollBy(-containerWidth, 0);
-  }
+arrowLeft.addEventListener('click', () => {
+  container.scrollLeft -= 300;
 });
 
-nextBtn.addEventListener('click', () => {
-  if (currentIndex < teamContainers.length - 1) {
-    currentIndex++;
-    teamFlex.scrollBy(containerWidth, 0);
-  }
+arrowRight.addEventListener('click', () => {
+  container.scrollLeft += 300;
 });
